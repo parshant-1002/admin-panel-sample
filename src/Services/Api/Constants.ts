b470deae52a3739dfce2f2c3d443e54899f5640a
parshant-1002 <parshant.sharma@chicmic.co.in>
2024-08-13 16:34:20 +0530

    chore: refactor

--- src/Services/Api/Constants.ts
+++ src/Services/Api/Constants.ts
@@ -23,7 +23,6 @@ export const API_END_POINTS = {
   REFERRAL_PACK_HISTORY: '/referralPack/history',
   REFERRED_USERS: '/referral/referredUsers',
   BID_PLAN: '/bidPlan',
-  USER_PRODUCTS: '/userProducts',
 };
 
 export const HTTPS_METHODS = {
