25bc04360aa87e44d647a1735c11a526e9510cb0
AbhayThakurChicmic <abhay.thakur@chicmic.co.in>
2024-09-05 15:47:51 +0530

    UI-UI-CHANGES

--- src/Shared/components/layouts/sidebar/Sidebar.tsx
+++ src/Shared/components/layouts/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useMemo, useState, useEffect } from 'react';
+import React, { useCallback, useMemo, useState, useEffect, useRef } from 'react';
 import { Accordion } from 'react-bootstrap';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { ROUTES } from '../../../constants';
 import SIDEBAR_NAV from './routes';
 import './style.scss';
@@ -15,8 +15,8 @@ interface SidebarItem {
 }
 
 function Sidebar() {
-  const location = useLocation();
-
+  // const location = useLocation();
+  const accordionRef = useRef<HTMLDivElement>(null); // Ref to track the accordion
   const [activeAccordion, setActiveAccordion] = useState<string | null>(null);
   const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 1200);
 
@@ -29,15 +29,31 @@ function Sidebar() {
     };
   }, []);
 
-  const handleButtonClick = (item: SidebarItem) => {
-    if (item.label === 'Logout') {
-      // Implement logout functionality
-    }
-  };
-
+  // Function to toggle accordion
   const toggleAccordion = (label: string) => {
     setActiveAccordion((prevActive) => (prevActive === label ? null : label));
   };
+
+  // Close the accordion if clicked outside
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    if (accordionRef.current && !accordionRef.current.contains(event.target as Node)) {
+      setActiveAccordion(null);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Add click listener when accordion is open and screen width is below 1200px (mobile)
+    if (activeAccordion && isMobile) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      // Cleanup listener when component unmounts or accordion closes
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [activeAccordion, isMobile, handleClickOutside]);
 
   const handleItemClick = () => {
     if (isMobile) {
@@ -45,83 +61,80 @@ function Sidebar() {
     }
   };
 
+  // Recursive function to generate the sidebar
   const mapChilds = useCallback(
-    (sidebar: SidebarItem, childrens: SidebarItem[]) => {
-      // const findIndex = childrens.findIndex(
-      //   (children) => children.route === location.pathname
-      // );
-      return (
-        <li className="nav-item" key={`sidebar-${sidebar.label}`}>
-          <Accordion activeKey={activeAccordion === sidebar.label ? '0' : ''}>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header onClick={() => toggleAccordion(sidebar.label)}>
-                <span className="curve-top" />
-                <span className="curve-bottom" />
-                {sidebar.iconClass && <i className={sidebar.iconClass} />}
-                {sidebar.icon && (
-                  <img width={18} className="me-2" src={sidebar.icon} />
-                )}
-                {sidebar.label}
-              </Accordion.Header>
-              <Accordion.Body className="nav-content collapse show">
-                <ul className="sub-menu">
-                  {childrens.map((children) => (
-                    <li key={`sidebar-${children.label}-`}>
-                      {children?.isOnClick ? (
-                        <button
-                          type="button"
-                          className="dropdown-item"
-                          onClick={handleItemClick}
-                        >
+    (sidebar: SidebarItem, childrens: SidebarItem[]) => (
+      <li className="nav-item" key={`sidebar-${sidebar.label}`}>
+        <Accordion activeKey={activeAccordion === sidebar.label ? '0' : ''}>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header onClick={() => toggleAccordion(sidebar.label)}>
+              <span className="curve-top" />
+              <span className="curve-bottom" />
+              {sidebar.iconClass && <i className={sidebar.iconClass} />}
+              {sidebar.icon && (
+                <img width={18} className="me-2" src={sidebar.icon} />
+              )}
+              {sidebar.label}
+            </Accordion.Header>
+            <Accordion.Body className="nav-content collapse show" ref={accordionRef}>
+              <ul className="sub-menu">
+                {childrens.map((children) => (
+                  <li key={`sidebar-${children.label}`}>
+                    {children.isOnClick ? (
+                      <button
+                        type="button"
+                        className="dropdown-item"
+                        onClick={handleItemClick}
+                      >
+                        {children.label}
+                      </button>
+                    ) : (
+                      <NavLink
+                        className="dropdown-item d-flex"
+                        to={children.route || ''}
+                        onClick={handleItemClick}
+                      >
+                        <>
+                          <figure className="me-3">
+                            <svg
+                              fill="#ffffff66"
+                              viewBox="0 0 56 56"
+                              height="15px"
+                              width="15px"
+                              xmlns="http://www.w3.org/2000/svg"
+                              stroke="#ffffff"
+                            >
+                              <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+                              <g
+                                id="SVGRepo_tracerCarrier"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                              <g id="SVGRepo_iconCarrier">
+                                <path d="M 27.9883 52 C 29.2774 52 29.9336 51.1328 31.2461 49.3516 L 45.2383 30.6719 C 45.8711 29.8047 46.2461 28.9375 46.2461 28 C 46.2461 27.0390 45.8711 26.1953 45.2383 25.3281 L 31.2461 6.6250 C 29.9336 4.8672 29.2774 4.0000 27.9883 4.0000 C 26.7227 4.0000 26.0664 4.8672 24.7539 6.6250 L 10.7617 25.3281 C 10.1289 26.1953 9.7539 27.0390 9.7539 28 C 9.7539 28.9375 10.1289 29.8047 10.7617 30.6719 L 24.7539 49.3516 C 26.0664 51.1328 26.7227 52 27.9883 52 Z M 27.9883 47.0547 C 27.8945 47.0547 27.8242 46.9844 27.7774 46.8672 L 14.2070 28.6328 C 13.9961 28.3750 13.9727 28.1875 13.9727 28 C 13.9727 27.8125 13.9961 27.6250 14.2070 27.3672 L 27.7774 9.1094 C 27.8242 9.0156 27.8945 8.9453 27.9883 8.9453 C 28.1055 8.9453 28.1758 9.0156 28.2227 9.1094 L 41.7930 27.3672 C 42.0039 27.6250 42.0274 27.8125 42.0274 28 C 42.0274 28.1875 42.0039 28.3750 41.7930 28.6328 L 28.2227 46.8672 C 28.1758 46.9844 28.1055 47.0547 27.9883 47.0547 Z" />
+                              </g>
+                            </svg>
+                          </figure>
                           {children.label}
-                        </button>
-                      ) : (
-                        <NavLink
-                          className="dropdown-item d-flex"
-                          to={children.route || ''}
-                          onClick={handleItemClick}
-                        >
-                          <>
-                            <figure className="me-3">
-                              <svg
-                                fill="#ffffff66"
-                                viewBox="0 0 56 56"
-                                height="15px"
-                                width="15px"
-                                xmlns="http://www.w3.org/2000/svg"
-                                stroke="#ffffff"
-                              >
-                                <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-                                <g
-                                  id="SVGRepo_tracerCarrier"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                />
-                                <g id="SVGRepo_iconCarrier">
-                                  <path d="M 27.9883 52 C 29.2774 52 29.9336 51.1328 31.2461 49.3516 L 45.2383 30.6719 C 45.8711 29.8047 46.2461 28.9375 46.2461 28 C 46.2461 27.0390 45.8711 26.1953 45.2383 25.3281 L 31.2461 6.6250 C 29.9336 4.8672 29.2774 4.0000 27.9883 4.0000 C 26.7227 4.0000 26.0664 4.8672 24.7539 6.6250 L 10.7617 25.3281 C 10.1289 26.1953 9.7539 27.0390 9.7539 28 C 9.7539 28.9375 10.1289 29.8047 10.7617 30.6719 L 24.7539 49.3516 C 26.0664 51.1328 26.7227 52 27.9883 52 Z M 27.9883 47.0547 C 27.8945 47.0547 27.8242 46.9844 27.7774 46.8672 L 14.2070 28.6328 C 13.9961 28.3750 13.9727 28.1875 13.9727 28 C 13.9727 27.8125 13.9961 27.6250 14.2070 27.3672 L 27.7774 9.1094 C 27.8242 9.0156 27.8945 8.9453 27.9883 8.9453 C 28.1055 8.9453 28.1758 9.0156 28.2227 9.1094 L 41.7930 27.3672 C 42.0039 27.6250 42.0274 27.8125 42.0274 28 C 42.0274 28.1875 42.0039 28.3750 41.7930 28.6328 L 28.2227 46.8672 C 28.1758 46.9844 28.1055 47.0547 27.9883 47.0547 Z" />
-                                </g>
-                              </svg>
-                            </figure>
-                            {children.label}
-                          </>
-                        </NavLink>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </li>
-      );
-    },
-    [location.pathname, activeAccordion, isMobile]
+                        </>
+                      </NavLink>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </li>
+    ),
+    [activeAccordion, handleItemClick]
   );
 
+  // Recursive function for the full sidebar
   const recursiveSidebar = useCallback(
     (menues?: SidebarItem[]) =>
       menues?.map((sidebar) => {
-        if (sidebar?.children) {
+        if (sidebar.children) {
           return mapChilds(sidebar, sidebar.children);
         }
         return (
@@ -143,7 +156,7 @@ function Sidebar() {
                 <button
                   className="nav-link"
                   type="button"
-                  onClick={() => handleButtonClick(sidebar)}
+                // onClick={() => handleButtonClick(sidebar)}
                 >
                   <em>
                     {sidebar.iconClass && <i className={sidebar.iconClass} />}
@@ -175,7 +188,6 @@ function Sidebar() {
         <h4 className="admin-brand-logo text-center d-none d-xl-block">
           Penny Auction
         </h4>
-        {/* <img src={Logo} alt="Logo" width={180} /> */}
       </Link>
       <ul className="sidebar-nav mt-3 pt-4 pt-xl-0" id="sidebar-nav">
         {sidebar}
